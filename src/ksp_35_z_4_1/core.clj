5ed711b9ca30897a7fe4b80a01bdca48f1dcdc65
Richard Tichý <tichy@artify.zone>
2023-03-18 17:19:17 +0100

    Implement solution

--- src/ksp_35_z_4_1/core.clj
+++ src/ksp_35_z_4_1/core.clj
@@ -1,6 +1,41 @@
-(ns ksp-35-z-4-1.core)
+(ns ksp-35-z-4-1.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def solve nil)
+(def solve-lights nil)
+
+(defn solve-file [input output]
+  (time (->> (slurp input)
+             (solve)
+             (#(str % "\n"))
+             (spit output))))
+
+(defn solve [lights]
+  (->> lights
+       (str/trim)
+       (mapv #(if (= % \X)
+                :broken
+                :off))
+       (solve-lights)
+       (map #(case %
+               :on \O
+               :off \-
+               :broken \X))
+       (reduce str)))
+
+(defn solve-lights [lights]
+  (-> lights
+      (assoc 0 :on)
+      (#(assoc % (dec (count %)) :on))
+      (#(persistent! (reduce (fn [lights i]
+                               (let [prev (nth lights (dec i))
+                                     curr (nth lights i)
+                                     next (nth lights (inc i))]
+                                 (if (not= curr :broken)
+                                   (if (or (not= prev :on)
+                                           (= next :broken))
+                                     (assoc! lights i :on)
+                                     (assoc! lights i :off)))
+                                 lights))
+                             (transient %)
+                             (range 1 (dec (count %))))))))
